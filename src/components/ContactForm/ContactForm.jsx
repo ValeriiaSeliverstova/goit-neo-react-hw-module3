29b9239b2a6d7cba9402046203eca9b30118d003
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-05-08 00:02:52 +0300

    feat: add formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,24 @@
+import css from "./ContactForm.module.css";
+import { Formik, Form, Field } from "formik";
+
 export default function ContactForm() {
-  return <div></div>;
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        number: "",
+      }}
+      onSubmit={() => {}}
+    >
+      <Form className={css.contactForm}>
+        <label htmlFor="username">Name</label>
+        <Field type="text" name="username" />
+        <label htmlFor="number">Number</label>
+        <Field type="tel" name="number" />
+        <button className={css.addContactButton} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
 }
