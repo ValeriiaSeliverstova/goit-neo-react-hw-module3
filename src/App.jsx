dcaee71fac86ef91d5ed38cdedd53f3b41fe36d3
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-05-08 00:50:27 +0300

    feat: get contacts from local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 export default function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : [];
+  });
 
   useEffect(() => {
     localStorage.setItem("contacts", JSON.stringify(contacts));
