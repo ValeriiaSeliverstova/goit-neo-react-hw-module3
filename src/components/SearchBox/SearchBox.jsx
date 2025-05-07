31f8a6359425418ac9851fd8e890683b0fee0007
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-05-07 23:04:11 +0300

    feat: add search field

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,3 +1,15 @@
 export default function SearchBox() {
-  return <div></div>;
+  return (
+    <div>
+      <label>
+        Find contacts by name
+        <input
+          type="text"
+          name="filter"
+          // value={filter}
+          // onChange={onFilterChange}
+        />
+      </label>
+    </div>
+  );
 }
