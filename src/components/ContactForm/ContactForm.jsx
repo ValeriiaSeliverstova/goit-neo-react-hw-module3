90efb8973ea6c07918c3c8351bc7ef09ef683225
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-05-08 00:28:09 +0300

    feat: add validation with yup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,49 @@
 import css from "./ContactForm.module.css";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useId } from "react";
 
 export default function ContactForm() {
+  const usernameId = useId();
+  const numberId = useId();
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .min(7, "Must be at least 7 digits")
+      .max(15, "Must be at most 15 digits")
+      .required("Required"),
+  });
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
   return (
     <Formik
       initialValues={{
         username: "",
         number: "",
       }}
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
     >
       <Form className={css.contactForm}>
-        <label htmlFor="username">Name</label>
-        <Field className={css.input} type="text" name="username" />
-        <label htmlFor="number">Number</label>
-        <Field className={css.input} type="tel" name="number" />
+        <label htmlFor={usernameId}>Name</label>
+        <Field
+          className={css.input}
+          type="text"
+          name="username"
+          id={usernameId}
+        />
+        <ErrorMessage className={css.error} name="username" component="span" />
+        <label htmlFor={numberId}>Number</label>
+        <Field className={css.input} type="tel" name="number" id={numberId} />
+        <ErrorMessage className={css.error} name="number" component="span" />
         <button className={css.addContactButton} type="submit">
           Add contact
         </button>
